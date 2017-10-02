b406aaeb010a0e02c8e54682d6c532ba890a1376
Surya <surya3997@gmail.com>
2017-10-02 21:33:14 +0530

    credential removed

--- includes/mydb.php
+++ includes/mydb.php
@@ -10,7 +10,7 @@ class DB    {
     private $dbHost = 'localhost';
     private $dbUser = 'ha';
     private $dbPass = 'ha';
-    private $dbName = 'ha17_checker';
+    private $dbName = 'ha';
   
     //This is the constructor
     function __construct() {
